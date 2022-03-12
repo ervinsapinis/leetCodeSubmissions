58277651d9f200e7954c5ec3a0962f51fddc4883
ervinsapinis <larmantine@gmail.com>
2022-03-12 16:31:11 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace leetCodeSubmissions
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
